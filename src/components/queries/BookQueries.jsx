217dd77732614c3fef2302cda0720c64d367595c
matiassemelman <matias.semelman@protonmail.com>
2022-07-08 10:09:22 -0600

    Adding HTML skeleton

--- src/components/queries/BookQueries.jsx
+++ src/components/queries/BookQueries.jsx
@@ -1,11 +1,27 @@
-const BookQueries = ({text, className, onClick}) => {
+const BookQueries = ({ text, className, onClick }) => {
   return (
     <>
-   <h1>BookQueries</h1>
-   <br />
-   <button className={className} onClick={onClick}>{text}</button>
+      <div className="container">
+        <div className="geoSuggest">
+          <h6>Lugar</h6>
+          <h5>El Chaltén</h5>
+        </div>
+        <div className="date-pickker">
+          <div className="arrival">
+            <h6>Fecha llegada</h6>
+            <h5>Julio, 15</h5>
+          </div>
+          <div className="departure">
+            <h6>Fecha partida</h6>
+            <h5>Seleccione fecha</h5>
+          </div>
+        </div>
+        <div class="button-container">
+          <button>Botón ya hecho</button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default BookQueries
+export default BookQueries;
