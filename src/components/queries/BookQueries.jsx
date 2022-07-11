ca0370a86d8f8522d8ae142e2e51cf1647413fb6
matiassemelman <matias.semelman@protonmail.com>
2022-07-11 10:21:03 -0600

    Feat: BookQuery first commit

--- src/components/queries/BookQueries.jsx
+++ src/components/queries/BookQueries.jsx
@@ -1,26 +1,25 @@
+import { useState } from "react";
+import { QueryBarContainer, QueryBarItem, QueryBarLabel } from "./styled";
+import Dropdown from './../Dropdown';
+
 const BookQueries = ({ text, className, onClick }) => {
+  const [destination, setDestination] = useState('¿A dónde vas?')
   return (
-    <>
-      <div className="container">
-        <div className="geoSuggest">
-          <h6>Lugar</h6>
-          <h5>El Chaltén</h5>
-        </div>
-        <div className="date-pickker">
-          <div className="arrival">
-            <h6>Fecha llegada</h6>
-            <h5>Julio, 15</h5>
-          </div>
-          <div className="departure">
-            <h6>Fecha partida</h6>
-            <h5>Seleccione fecha</h5>
-          </div>
-        </div>
-        <div class="button-container">
-          <button>Botón ya hecho</button>
-        </div>
-      </div>
-    </>
+    <QueryBarContainer>
+      <QueryBarItem>
+        <QueryBarLabel>Lugar</QueryBarLabel>
+       <Dropdown/>
+
+      </QueryBarItem>
+      <QueryBarItem>
+        <QueryBarLabel>Fecha llegada</QueryBarLabel>
+      </QueryBarItem>
+      <QueryBarItem>
+        <QueryBarLabel>Fecha Partida</QueryBarLabel>
+      </QueryBarItem>
+
+      <QueryBarItem></QueryBarItem>
+    </QueryBarContainer>
   );
 };
 
