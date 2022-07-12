9c1c2e5fbaa2fee8a90aef7b827df6ac28dcb38e
matiassemelman <matias.semelman@protonmail.com>
2022-07-12 11:47:37 -0600

    Styles: Dropdown Header improved

--- src/components/dropdown/styled.jsx
+++ src/components/dropdown/styled.jsx
@@ -2,18 +2,14 @@ import styled from "styled-components";
 
 
 export const DropDownContainer = styled("div")`
-width: 10.5em;
-margin: 0 auto;
+
 `;
 
 export const DropDownHeader = styled("div")`
-margin-bottom: 0.8em;
-padding: 0.4em 2em 0.4em 1em;
-box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
 font-weight: 500;
-font-size: 1.3rem;
-color: #3faffa;
-background: #ffffff;
+color: grey;
+font-size: 1.1rem;
+
 `;
 
 export const DropDownListContainer = styled("div")``;
