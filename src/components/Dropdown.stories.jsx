53372977906582db2e635027fa3569e580cd85b1
matiassemelman <matias.semelman@protonmail.com>
2022-07-12 10:40:17 -0600

    Clean comments

--- src/components/Dropdown.stories.jsx
+++ src/components/Dropdown.stories.jsx
@@ -6,12 +6,7 @@ export default {
  
 }
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Dropdown {...args} />;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-    text: "Change me"
-};
 
